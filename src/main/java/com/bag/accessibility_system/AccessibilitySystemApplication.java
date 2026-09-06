032e56c2bb4ad4b985e190a5c518499ceef85f79
Brayam Ayala <b.ayalaga06@gmail.com>
2026-09-06 12:19:55 -0500

    chore: initial project setup from spring initializr

---
+++ src/main/java/com/bag/accessibility_system/AccessibilitySystemApplication.java
@@ -0,0 +1,13 @@
+package com.bag.accessibility_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibilitySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibilitySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bag/accessibility_system/AccessibilitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bag.accessibility_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibilitySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
